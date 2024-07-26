e08afa10371b90ddbf41db2497f762b9401ff60a
mohsinalian <123172473+mohsinalian@users.noreply.github.com>
2024-07-26 17:42:26 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
-const io = require("socket.io")(8000);
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+
+const httpServer = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Socket.IO server");
+});
+const io = new Server(httpServer);
+
 const user = {};
 
 io.on("connection", (socket) => {
@@ -14,9 +22,13 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("disconnect", (message) => {
+  socket.on("disconnect", () => {
     socket.broadcast.emit("left", user[socket.id]);
-
     delete user[socket.id];
   });
 });
+
+// Vercel's function export
+module.exports = (req, res) => {
+  httpServer.emit("request", req, res);
+};
