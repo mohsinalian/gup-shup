d7a9e302c58a103417038f40f09ddf30583bcc3d
mohsinalian <123172473+mohsinalian@users.noreply.github.com>
2024-07-29 13:27:59 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: "*", // Allow requests from all origins
     methods: ["GET", "POST"],
-    credentials: false,
+    credentials: true,
   }
 });
 
