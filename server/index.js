93365aad7ce725982435112364c7f0b731263ee5
mohsinalian <123172473+mohsinalian@users.noreply.github.com>
2024-07-29 10:37:26 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,17 @@
-const { Server } = require("socket.io");
-const { createServer } = require("http");
-
-const httpServer = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Socket.IO server is running");
+const port = process.env.PORT || 8000;
+const io = require("socket.io")(port, {
+  cors: {
+    origin: "*", // Allow requests from all origins
+  },
 });
-
-const io = new Server(httpServer);
 
 const user = {};
 
 io.on("connection", (socket) => {
-  console.log(`New connection: ${socket.id}`);
-
+  console.log("New user connected");
   socket.on("new-user-joined", (name) => {
     user[socket.id] = name;
     socket.broadcast.emit("user-joined", name);
-    console.log(`${name} has joined`);
   });
 
   socket.on("send", (message) => {
@@ -27,20 +22,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log(`User ${user[socket.id]} disconnected`);
     socket.broadcast.emit("left", user[socket.id]);
     delete user[socket.id];
   });
 });
-
-// Log message to indicate the server setup
-console.log("Server setup completed, ready to handle requests.");
-
-// Vercel's function export
-module.exports = (req, res) => {
-  httpServer.emit("request", req, res);
-};
-module.exports = (req, res) => {
-  res.status(200).send('Hello, world!');
-};
-
