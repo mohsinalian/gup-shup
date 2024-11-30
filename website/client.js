e28f7037c74a6d2491ed47e49f34271c970f7088
mohsinalian <123172473+mohsinalian@users.noreply.github.com>
2024-11-30 14:50:11 +0500

    Update client.js

--- website/client.js
+++ website/client.js
@@ -1,5 +1,5 @@
 // const socket = io("https://gup-shup-app.vercel.app/");
-const socket = io('https://gup-shup-server.vercel.app/', {
+const socket = io('https://gup-shup-server-production.up.railway.app/', {
   transports: ['websocket'], // Ensure you're using WebSocket transport
 });
 
