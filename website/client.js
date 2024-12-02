317b597bad105c93025fe9280e459cc8d65f45f5
mohsinalian <123172473+mohsinalian@users.noreply.github.com>
2024-12-02 11:03:56 +0500

    Update client.js

--- website/client.js
+++ website/client.js
@@ -6,15 +6,28 @@ const socket = io("https://gup-shup-server-production.up.railway.app/", {
 const form = document.querySelector("#send-container");
 const messageInput = document.querySelector("#messageInp");
 const messageContainer = document.querySelector(".container");
+messageInput.focus();
 
 const audio = new Audio("sound/ding2-89720.mp3");
 
-const append = function (message, position) {
-  const messageElement = document.createElement("p");
-  messageElement.innerText = message;
-  messageElement.classList.add("message");
+const append = function (name, message, position) {
+  //create elements
+  const messageElement = document.createElement("div");
+  const userName = document.createElement("p");
+  const messageText = document.createElement("p");
+  // Assigning Values
+  userName.innerText = name;
+  messageText.innerText = message;
+  //  assigning classes
+  messageElement.classList.add("messageBox");
   messageElement.classList.add(position);
+  userName.classList.add("name");
+  messageText.classList.add("message");
+  // Adding Elments to dome
+  messageElement.append(userName);
+  messageElement.append(messageText);
   messageContainer.append(messageElement);
+  // check
   if (position == "left") {
     audio.play();
   }
@@ -23,7 +36,7 @@ const append = function (message, position) {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const message = messageInput.value;
-  append(` ${message}`, "right");
+  append("", message, "right");
   socket.emit("send", message);
   messageInput.value = "";
 });
@@ -32,13 +45,13 @@ const named = prompt("Enter your name to join");
 socket.emit("new-user-joined", named);
 
 socket.on("user-joined", (name) => {
-  append(`${name} Joined chat!`, "left");
+  append(name, "Joined chat!", "left");
 });
 
 socket.on("receive", (data) => {
-  append(`${data.name} :\n \t\t${data.message}`, "left");
+  append(data.name, data.message, "left");
 });
 
 socket.on("left", (name) => {
-  append(`${name} :\n\n Left the Chat!`, "left");
+  append(name, "Left the Chat!", "left");
 });
